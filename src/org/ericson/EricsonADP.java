debfd69d10d5adce39c5b9f05b2a205e35d03a9d
karuppasamythiyagu <ajsamy2156@gmail.com>
2022-06-07 12:17:51 +0530

    some code were added

--- src/org/ericson/EricsonADP.java
+++ src/org/ericson/EricsonADP.java
@@ -1,9 +1,14 @@
 package org.ericson;
 
 public class EricsonADP {
-	
+
 	private void employeeName() {
 		System.out.println("Employee Name:Arun");
+
+	}
+
+	private void employeeId() {
+		System.out.println("Employee_Id:ME8765");
 
 	}
 
